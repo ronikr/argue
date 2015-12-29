aae64acabdca87ade955a9201586f9f399658b6c
Roni <ronikrakover@gmail.com>
2015-12-29 11:09:51 +0200

    j

--- app/home/home.js
+++ app/home/home.js
@@ -10,15 +10,17 @@
     });
 
 
-    module.controller('HomeCtrl', function (ChatFactory) {
+    module.controller('HomeCtrl', function (ChatFactory , $routeParams) {
 
       this.argues = ChatFactory.getArgues();
 
         //console.log(this.argues);
 
 
-        this.povClicked = function(pov){
+        this.povClicked = function(pov,argue){
             ChatFactory.setPov(pov);
+            ChatFactory.setArgue(argue);
+            window.location.assign("#/chat/");
             //console.log('homeCtrl', pov);
 
         };
